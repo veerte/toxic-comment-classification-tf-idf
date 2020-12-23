d553cf9f43a01f283c4c8ad48b729cc801e51290
veerte <matteusz.lichota@gmail.com>
2020-12-23 18:25:35 +0100

    add script for downloading data

---
+++ download_data.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+curl https://raw.githubusercontent.com/Imperial-College-Data-Science-Society/Challenges/master/toxic-comment-classification/train.csv -o train.csv
+curl https://raw.githubusercontent.com/Imperial-College-Data-Science-Society/Challenges/master/toxic-comment-classification/test_cleaned.csv -o test_cleaned.csv
